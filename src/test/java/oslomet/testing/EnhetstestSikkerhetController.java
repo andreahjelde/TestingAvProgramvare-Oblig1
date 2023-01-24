1892b50d98ff14dce8b1f7e2ecc3c62ad2c08910
KristineSkovly <89635723+KristineSkovly@users.noreply.github.com>
2023-01-24 12:19:58 +0100

    Update EnhetstestSikkerhetController.java

--- src/test/java/oslomet/testing/EnhetstestSikkerhetController.java
+++ src/test/java/oslomet/testing/EnhetstestSikkerhetController.java
@@ -1,29 +1,36 @@
 package oslomet.testing;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+import org.springframework.mock.web.MockHttpSession;
+import oslomet.testing.DAL.BankRepository;
+import oslomet.testing.Sikkerhet.Sikkerhet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+//Siggen og Krissa sin del :-)
+
+@RunWith(MockitoJUnitRunner.class)
 public class EnhetstestSikkerhetController {
-
-
-    //Sigurd og Krissa sin del
-
-    //hallaballa
+    
 
 
 
-    //HALLA DAMER
-
-
-    //Halla damer - Kristine
-
-
-    //:)
-
-
-    //2. halla damer - Sigurd
 
 
 
-    // ^__^
-
-//Andrea
 }
 
