ec8aee0002a99f22896e018ca9b6bb862e0f55b0
mjenssen1 <s342727@oslomet.no>
2023-01-24 09:49:04 +0100

    Update EnhetstestAdminKontoController.java
    Enhetstester ferdigstilt for AdminKontoControlleren.

--- src/test/java/oslomet/testing/EnhetstestAdminKontoController.java
+++ src/test/java/oslomet/testing/EnhetstestAdminKontoController.java
@@ -19,6 +19,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
@@ -35,9 +36,11 @@ public class EnhetstestAdminKontoController {
     @Mock
     private Sikkerhet sjekk;
 
-    //Sjekker for å hente konto hvis man ER logget inn.
+    //Sjekker for å hente konto hvis bruker er logget inn.
     @Test
-    public void hentAlleKonti_loggetInn(){
+    public void hentAlleKonti_loggetInnGodkjent(){
+
+        // Oppretter en liste av typen Konto.
         List<Konto> konti = new ArrayList<>();
 
         Konto konti1 = new Konto("2805199609234", "01010123560",
@@ -59,9 +62,9 @@ public class EnhetstestAdminKontoController {
         assertEquals(konti, resultat);
     }
 
-    //Sjekker hent alle kontoer hvis man IKKE er logget inn.
+    //Sjekker hent alle kontoer hvis bruker ikke er logget inn. Skal returnere null/ingenting.
     @Test
-    public void hentAlleKonti_ikkeLoggetInn(){
+    public void hentAlleKonti_ikkeLoggetInnFeil(){
 
         //Arrange
         when(sjekk.loggetInn()).thenReturn(null);
@@ -73,33 +76,117 @@ public class EnhetstestAdminKontoController {
         assertNull(resultat);
     }
 
+    // Sjekker registrert konto hvis bruker er logget inn.
     @Test
-    public void registrerKonto_loggetInn(){
+    public void registrerKonto_loggetInnGodkjent(){
+
+        //Oppretter en konto
+        Konto konto1 = new Konto("2805199609234", "01010123560",
+                10_000, "Sparekonto", "NOK", null);
+
+        //Mock
+        when(sjekk.loggetInn()).thenReturn("2805199609234");
+
+        //Mock
+        when(repository.registrerKonto(konto1)).thenReturn("Godkjent");
+
+        // Act
+        String resultat = adminKontoController.registrerKonto(konto1);
+
+        // Assert
+        assertEquals("Godkjent", resultat);
 
     }
 
+    // Sjekker registrertKonto hvis ikke bruker er logget inn. Skal returnere "Ikke innlogget".
     @Test
-    public void registrerKonto_ikkeLoggetInn(){
+    public void registrerKonto_ikkeLoggetInnFeil(){
+
+        Konto konto1 = new Konto("2805199609234", "01010123560",
+                10_000, "Sparekonto", "NOK", null);
+
+        // Arrange
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        // Act
+        String resultat = adminKontoController.registrerKonto(konto1);
+
+        // Assert
+        assertEquals("Ikke innlogget", resultat);
 
     }
 
+    // Sjekker for endreKonto hvis bruker er logget inn.
     @Test
-    public void endreKonto_loggetInn(){
+    public void endreKonto_loggetInnGodkjent(){
+
+        // Oppretter en konto av typen "Konto".
+        Konto konto1 = new Konto("2805199609234", "01010123560",
+                10_000, "Sparekonto", "NOK", null);
+
+        when(sjekk.loggetInn()).thenReturn("2805199609234");
+
+        when(repository.endreKonto(any(Konto.class))).thenReturn("Godkjent");
+
+        // Act
+        String resultat = adminKontoController.endreKonto(konto1);
+
+        // Assert
+        assertEquals("Godkjent", resultat);
 
     }
 
+    // Sjekker for endreKonto hvis bruker ikke er logget inn.
     @Test
-    public void endreKonto_ikkeLoggetInn(){
+    public void endreKonto_ikkeLoggetInnFeil(){
+
+        Konto konto1 = new Konto("2805199609234", "01010123560",
+                10_000, "Sparekonto", "NOK", null);
+
+        // Arrange
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        // Act
+        String resultat = adminKontoController.endreKonto(konto1);
+
+        // Assert
+        assertEquals("Ikke innlogget", resultat);
+    }
+
+    // Sjekker for slett konto hvis bruker er logget inn.
+    @Test
+    public void slettKonto_loggetInnGodkjent(){
+
+        Konto konto1 = new Konto("2805199609234", "01010123560",
+                10_000, "Sparekonto", "NOK", null);
+
+        when(sjekk.loggetInn()).thenReturn("2805199609234");
+
+        when(repository.slettKonto(anyString())).thenReturn("Godkjent");
+
+        // Act
+        String resultat = adminKontoController.slettKonto(konto1.getKontonummer());
+
+        // Assert
+        assertEquals("Godkjent", resultat);
 
     }
 
+    // Sjekker for slett konto hvis bruker ikke er logget inn.
     @Test
-    public void slettKonto_loggetInn(){
+    public void slettKonto_ikkeLoggetInnFeil(){
 
-    }
+        Konto konto1 = new Konto("2805199609234", "01010123560",
+                10_000, "Sparekonto", "NOK", null);
 
-    @Test
-    public void slettKonto_ikkeLoggetInn(){
+        // Arrange
+        when(sjekk.loggetInn()).thenReturn(null);
 
+        // Act
+        String resultat = adminKontoController.slettKonto(konto1.getKontonummer());
+
+        // Assert
+        assertEquals("Ikke innlogget", resultat);
+        
     }
 }
