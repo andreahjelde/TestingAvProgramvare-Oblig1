be584db43d3725fa3ac2e69773049bddec37320e
siggen1 <89570178+siggen1@users.noreply.github.com>
2023-02-07 14:30:16 +0100

    Update EnhetstestSikkerhetController.java

--- src/test/java/oslomet/testing/EnhetstestSikkerhetController.java
+++ src/test/java/oslomet/testing/EnhetstestSikkerhetController.java
@@ -32,51 +32,52 @@ public class EnhetstestSikkerhetController {
     private BankRepository repository;
 
     @Mock
-    // denne skal Mock'es
+    // denne skal også Mock'es
     private MockHttpSession session;
 
-   @Before
-   public void initSessions(){
-       Map<String,Object> attributes = new HashMap<String,Object>();
+    //Skjønner ikke helt denne, fått hjelp av TA. Må ha det med for å kjøre integrasjonstesting på sessions.
+    @Before
+    public void initSessions() {
+        Map<String, Object> attributes = new HashMap<String, Object>();
 
-       doAnswer(new Answer<Object>(){
-           @Override
-           public Object answer(InvocationOnMock invocation) throws Throwable {
-               String key = (String) invocation.getArguments()[0];
-               return attributes.get(key);
-           }
-       }).when(session.getAttribute(anyString()));
-       doAnswer(new Answer<Object>(){
-           @Override
-           public Object answer(InvocationOnMock invocation) throws Throwable {
-               String key = (String) invocation.getArguments()[0];
-               Object value = invocation.getArguments()[1];
-               attributes.put(key, value);
-               return null;
-           }
-       }).when(session).setAttribute(anyString(),any());
-   }
+        doAnswer(new Answer<Object>() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                String key = (String) invocation.getArguments()[0];
+                return attributes.get(key);
+            }
+        }).when(session).getAttribute(anyString());
 
-    //Sjekk logInn
+        doAnswer(new Answer<Object>() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                String key = (String) invocation.getArguments()[0];
+                Object value = invocation.getArguments()[1];
+                attributes.put(key, value);
+                return null;
+            }
+        }).when(session).setAttribute(anyString(), any());
+    }
+
+    /*------------------------ LOGG INN --------------------------------*/
     //Tester sjekkLoggInn metoden i sikkerhets controlleren (Sikkerhet.java)
-    //Logget inn = "OK"
+    //Hvis bruker er logget inn = "OK"
 
     @Test
-
-    public void sjekkLoggInn_LoggetInn_OK(){
-        //arrange:
+    public void sjekkLoggInn_LoggetInn_OK() {
+        //arrange: sett opp testobjektene og klargjør forutsetningene for testen
         //arrange brukes for å teste setup’en
         //man burde lage hvert nødvendige objekt her om det trengs
         //I tillegg burde du legge til mocks for avhengigheter (when – then)
 
-        when(repository.sjekkLoggInn(anyString(),anyString())).thenReturn("OK");
+        when(repository.sjekkLoggInn(anyString(), anyString())).thenReturn("OK");
 
-        //act:
+        //act:(utfør selve testens arbeid)
         //Act brukes for å kalle test-metoden
 
         String resultat = sikkerhetsController.sjekkLoggInn("12345678901", "HeiHei");
 
-        //assert:
+        //assert (bekreft resultatet)
         //Assert er «hjertet av testen»
         //Her kan vi verifisere at resultatet av metodekallet vi gjør returnerer det vi ønsker at det skal
 
@@ -87,15 +88,15 @@ public class EnhetstestSikkerhetController {
     //Tester sjekk LoggInn (Ikke Logget Inn - Feil Personnummer Eller Passord)
     @Test
 
-    public void sjekkLoggInn_FeilPersonnummerEllerPassord(){
-       //arrange
-        Mockito.when(repository.sjekkLoggInn(anyString(),anyString())).thenReturn("Feil i personnummer eller passord");
+    public void sjekkLoggInn_FeilPersonnummerEllerPassord() {
+        //arrange
+        Mockito.when(repository.sjekkLoggInn(anyString(), anyString())).thenReturn("Feil i personnummer eller passord");
 
         //act
         String resultat = sikkerhetsController.sjekkLoggInn("12345678908", "HeiHeiHei");
 
         //assert
-        assertEquals("Feil i personnummer eller passord",resultat);
+        assertEquals("Feil i personnummer eller passord", resultat);
     }
 
     // Tester sjekk LoggInn (Ikke Logget Inn - Feil Med Regex Personnummer)
@@ -131,33 +132,31 @@ public class EnhetstestSikkerhetController {
 
     //Tester sjekk LoggInn (Ikke Logget Inn - Feil Med Regex Passord)
 
-      @Test
-      public void sjekkLoggInn_FeilMedRegExPassord(){
-       //act
+    @Test
+    public void sjekkLoggInn_FeilMedRegExPassord() {
+        //act
 
-       //Tomt passord
-       String resultat1 = sikkerhetsController.sjekkLoggInn("123456789091", "");
+        //Tomt passord
+        String resultat1 = sikkerhetsController.sjekkLoggInn("12345678901", "");
 
-       //For langt passord
-          String resultat2 = sikkerhetsController.sjekkLoggInn("12345678901","HeiPåDegDuErTøff123455678901234567890");
+        //For langt passord
+        String resultat2 = sikkerhetsController.sjekkLoggInn("12345678901", "HeiPåDegDuErTøff123455678901234567890");
 
-       //For kort passord
-       String resultat3 = sikkerhetsController.sjekkLoggInn("12345678901","Hei1");
+        //For kort passord
+        String resultat3 = sikkerhetsController.sjekkLoggInn("12345678901", "Hei1");
 
-       //assert
-          assertEquals("Feil i passord", resultat1);
-          assertEquals("Feil i passord", resultat2);
-          assertEquals("Feil i passord", resultat3);
-      }
-
-        /*------ Logg ut ------*/
-
-    /* Tester LoggUt (Ikke nødvendig siden denne metoden i
-     sikkerhetscontrolleren er void ) */
+        //assert
+        assertEquals("Feil i passord", resultat1);
+        assertEquals("Feil i passord", resultat2);
+        assertEquals("Feil i passord", resultat3);
+    }
 
 
-        @Test
-        public void LoggUt(){
+    /*------------------------ LOGG UT --------------------------------*/
+
+    /* Tester metoden "loggUt" */
+    @Test
+    public void LoggUt(){
         //arrange
         session.setAttribute("Innlogget", null);
         sikkerhetsController.loggUt();
@@ -167,34 +166,34 @@ public class EnhetstestSikkerhetController {
 
         //assert
         assertNull(resultat);
+    }
 
-        }
 
+    /*------------------------ LOGG INN ADMIN --------------------------------*/
 
-        /*------ LoggInn Admin ------*/
+    // Tester LoggInn Admin
+    // Logget Inn = OK
 
-        // Tester LoggInn Admin (Logget Inn - OK)
+    @Test
+    public void LoggInnAdmin_LoggetInn_OK() {
 
-        @Test
-        public void LoggInnAdmin_LoggetInn_OK(){
+        //arrange
+        session.setAttribute("Innlogget", "Admin");
 
-            //arrange
-            session.setAttribute("Innlogget", "Admin");
+        //act
+        String resultat = sikkerhetsController.loggInnAdmin("Admin", "Admin");
 
-            //act
-            String resultat = sikkerhetsController.loggInnAdmin("Admin", "Admin");
+        //assert
+        assertEquals("Logget inn", resultat);
 
-            //assert
-            assertEquals("Logget inn", resultat);
+    }
 
-        }
+    // Tester LoggInn Admin (Ikke Logget Inn - Feil)
 
-        // Tester LoggInn Admin (Ikke Logget Inn - Feil)
-
-        @Test
-        public void LoggInnAdmin_IkkeLoggetInn(){
-            //arrange
-        session.setAttribute("Innlogget",null);
+    @Test
+    public void LoggInnAdmin_IkkeLoggetInn() {
+        //arrange
+        session.setAttribute("Innlogget", null);
 
         //act
         String resultat = sikkerhetsController.loggInnAdmin("", "");
@@ -202,30 +201,30 @@ public class EnhetstestSikkerhetController {
         //assert
         assertEquals("Ikke logget inn", resultat);
 
-        }
+    }
 
-        /*------ Logget Inn ------*/
+    /*------ Logget Inn ------*/
 
-        // Tester Logget Inn (Logget Inn - OK)
+    // Tester Logget Inn (Logget Inn - OK)
 
     @Test
-    public void LoggetInn_LoggInn_OK(){
+    public void LoggetInn_LoggInn_OK() {
         //arrange
-        session.setAttribute("Innlogget","12345678901");
+        session.setAttribute("Innlogget", "12345678901");
 
         //act
         String resultat = sikkerhetsController.loggetInn();
 
         //assert
-        assertEquals("12345678901",resultat);
+        assertEquals("12345678901", resultat);
     }
 
     // Tester Logget Inn (Logget Inn - Feil)
 
     @Test
-    public void LoggetInn_IkkeLoggetInn(){
+    public void LoggetInn_IkkeLoggetInn() {
         //arrange
-        session.setAttribute(null,null);
+        session.setAttribute(null, null);
 
         //act
         String resultat = sikkerhetsController.loggetInn();
