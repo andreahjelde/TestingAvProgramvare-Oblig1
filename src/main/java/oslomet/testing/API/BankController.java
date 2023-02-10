86940c2a95c47210c3b69d8fe185d98857d301d2
siggen1 <89570178+siggen1@users.noreply.github.com>
2023-02-10 12:21:55 +0100

    lagt til det som trengs for å initialisere database

--- src/main/java/oslomet/testing/API/BankController.java
+++ src/main/java/oslomet/testing/API/BankController.java
@@ -11,6 +11,7 @@ import oslomet.testing.Models.Kunde;
 import oslomet.testing.Models.Transaksjon;
 import oslomet.testing.Sikkerhet.Sikkerhet;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 @RestController
@@ -96,5 +97,15 @@ public class BankController {
                 return repository.endreKundeInfo(innKunde);
             }
         return null;
+    }
+
+
+    //FOR INTEGRASJONSTESTING - For å kunne initere databasen fra integrasjonstesten trenger vi denne koden.
+    @Autowired
+    private DataSource dataSource;
+
+    @GetMapping("/initDB")
+    public String initDB(){
+        return repository.initDB(dataSource);
     }
 }
